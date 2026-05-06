ca9178185e826f371dd54257f915d82189b2ad28
Mudassir Hassan <91305738+Mudassir55@users.noreply.github.com>
2026-05-06 12:51:38 +0500

    Fixed NLTK deployment issue

--- utils/keyword_extractor.py
+++ utils/keyword_extractor.py
@@ -1,17 +1,31 @@
 import re
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-stop_words = set(stopwords.words('english'))
+# Download required NLTK data if missing
+try:
+    stop_words = set(stopwords.words('english'))
+except LookupError:
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    stop_words = set(stopwords.words('english'))
 
 
 def clean_text(text):
     text = text.lower()
 
+    # Remove special characters
     text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
 
-    words = word_tokenize(text)
+    # Tokenize text
+    try:
+        words = word_tokenize(text)
+    except LookupError:
+        nltk.download('punkt')
+        words = word_tokenize(text)
 
+    # Remove stopwords and short words
     filtered_words = [
         word for word in words
         if word not in stop_words and len(word) > 2
